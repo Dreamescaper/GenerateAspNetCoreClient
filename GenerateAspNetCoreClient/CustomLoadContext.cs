b800621a086e6e95850b54255e8baad795e8c581
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2021-02-28 01:50:16 +0200

    Add an ugly workaround to handle older ASP.NET versions

--- GenerateAspNetCoreClient/CustomLoadContext.cs
+++ GenerateAspNetCoreClient/CustomLoadContext.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
+using System.Text.Json;
 
 namespace GenerateAspNetCoreClient
 {
     internal class CustomLoadContext : AssemblyLoadContext
     {
         private readonly AssemblyDependencyResolver dependencyResolver;
+        private readonly string frameworkPath;
         private readonly Assembly sharedAssemply;
 
         public CustomLoadContext(string assemblyPath, Assembly sharedAssembly)
         {
             dependencyResolver = new AssemblyDependencyResolver(assemblyPath);
+            frameworkPath = GetFrameworkPath(assemblyPath);
             sharedAssemply = sharedAssembly;
         }
 
@@ -19,8 +24,7 @@ namespace GenerateAspNetCoreClient
         {
             if (assemblyName.FullName == sharedAssemply.FullName)
                 return sharedAssemply;
-
-            var path = dependencyResolver.ResolveAssemblyToPath(assemblyName);
+            var path = ResolveAssemblyToPath(assemblyName);
 
             if (path == null && fallbackToDefault)
             {
@@ -31,6 +35,24 @@ namespace GenerateAspNetCoreClient
             }
 
             return path != null ? LoadFromAssemblyPath(path) : null;
+        }
+
+        private string ResolveAssemblyToPath(AssemblyName assemblyName)
+        {
+            var path = dependencyResolver.ResolveAssemblyToPath(assemblyName);
+
+            if (path != null)
+                return path;
+
+            if (frameworkPath != null)
+            {
+                var frameworkAssemblyPath = Path.Combine(frameworkPath, assemblyName.Name + ".dll");
+
+                if (File.Exists(frameworkAssemblyPath))
+                    return frameworkAssemblyPath;
+            }
+
+            return null;
         }
 
         protected override Assembly Load(AssemblyName assemblyName)
@@ -45,6 +67,66 @@ namespace GenerateAspNetCoreClient
             return resolvedPath == null
                 ? IntPtr.Zero
                 : LoadUnmanagedDllFromPath(resolvedPath);
+        }
+
+        private static string GetFrameworkPath(string assemblyPath)
+        {
+            // Unfortunately, AssemblyDependencyResolver does not resolve framework references.
+            // In case of matching TargetFramework for generator tool and web project we can simply fallback to 
+            // AssemblyLoadContext.Default, but that doesn't work for older ASP.NET versions (e.g. .NET Core 3.1).
+            // Therefore we attempt to resolve framework assembly path manually - by parsing runtimeconfig.json, and finding
+            // framework folder.
+            // Any better ideas?...
+
+            try
+            {
+                var runtimeConfigPath = Path.Combine(Path.GetDirectoryName(assemblyPath),
+                    Path.GetFileNameWithoutExtension(assemblyPath) + ".runtimeconfig.json");
+
+                if (!File.Exists(runtimeConfigPath))
+                    return null;
+
+                var runtimeConfig = JsonDocument.Parse(File.ReadAllText(runtimeConfigPath));
+
+                var framework = runtimeConfig.RootElement
+                    .GetProperty("runtimeOptions")
+                    .GetProperty("framework");
+
+                // e.g. name = Microsoft.AspNetCore.App, version = 3.1.0.
+                var name = framework.GetProperty("name").GetString();
+                var version = framework.GetProperty("version").GetString();
+
+                if(name != "Microsoft.AspNetCore.App")
+                {
+                    // Do not attempt to resolve Microsoft.NETCore.App references.
+                    // This block is only expected to execute for ASP.Net 2.1 projects (before FrameworkReferences were introduced).
+                    return null;
+                }
+
+                var sharedDirectoryPath =
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(typeof(object).Assembly.Location)));
+
+                var frameworkVersionDirectories = new DirectoryInfo(Path.Combine(sharedDirectoryPath, name)).GetDirectories().Reverse();
+
+                // Attempt to find strict match first, but fallback to fuzzy match (e.g. 3.1.12 instead of 3.1.0).
+                while (!string.IsNullOrEmpty(version))
+                {
+                    var versionDirectory = frameworkVersionDirectories.FirstOrDefault(d => d.Name.StartsWith(version));
+
+                    if (versionDirectory != null)
+                        return versionDirectory.FullName;
+
+                    version = version.Substring(0, version[0..^1].LastIndexOf('.') + 1);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to find framework directory: " + e.ToString());
+            }
+
+            return null;
         }
     }
 }
