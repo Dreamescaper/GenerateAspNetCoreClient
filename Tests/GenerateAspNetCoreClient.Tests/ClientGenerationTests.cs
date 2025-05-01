80ca4e330d77a82d09f514bf2527665299ecfb0e
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2025-05-01 14:04:46 +0300

    Fix double using directive. Use Verify for snapshots.

--- Tests/GenerateAspNetCoreClient.Tests/ClientGenerationTests.cs
+++ Tests/GenerateAspNetCoreClient.Tests/ClientGenerationTests.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
+﻿using System.IO;
+using System.Threading.Tasks;
 using DotNet.Cli.Build;
 using GenerateAspNetCoreClient.Options;
 using NUnit.Framework;
+using VerifyNUnit;
 
 namespace GenerateAspNetCoreClient.Tests
 {
     [NonParallelizable]
     public class ClientGenerationTests
     {
-        private static readonly string _snapshotsPath = Path.Combine("..", "..", "..", "__snapshots__", "{0}");
-
         private static readonly string _inputPath = Path.Combine("..", "..", "..", "..", "{0}", "{0}.csproj");
         private static readonly string _outPath = Path.Combine("..", "..", "..", "..", "OutputTest", "Client");
         private static readonly string _outProjectPath = Path.Combine("..", "..", "..", "..", "OutputTest", "OutputProject.csproj");
-
-        private static readonly bool _regenerateSnapshots = false;
 
         [TearDown]
         public void CleanOutput()
@@ -27,7 +23,7 @@ namespace GenerateAspNetCoreClient.Tests
         [TestCase("TestWebApi.Controllers")]
         [TestCase("TestWebApi.Versioning")]
         [TestCase("TestWebApi.MinimalApi")]
-        public void GenerationTest(string testProjectName)
+        public async Task GenerationTest(string testProjectName)
         {
             var options = new GenerateClientOptions
             {
@@ -35,16 +31,15 @@ namespace GenerateAspNetCoreClient.Tests
                 OutPath = _outPath,
                 Namespace = "Test.Name.Space",
             };
-
+        
             Program.CreateClient(options);
-
+        
             Assert.That(() => Project.FromPath(_outProjectPath).Build(), Throws.Nothing);
-            AssertSnapshotMatch(testProjectName);
+            await Verifier.VerifyDirectory(_outPath);
         }
 
-
         [Test]
-        public void GenerationTest_UseApiResponses()
+        public async Task GenerationTest_UseApiResponses()
         {
             var options = new GenerateClientOptions
             {
@@ -57,61 +52,25 @@ namespace GenerateAspNetCoreClient.Tests
             Program.CreateClient(options);
 
             Assert.That(() => Project.FromPath(_outProjectPath).Build(), Throws.Nothing);
-            AssertSnapshotMatch("TestWebApi.Controllers.UseApiResponses");
+            await Verifier.VerifyDirectory(_outPath);
         }
-
-        private static void RegenerateSnapshots(string testProjectName)
+        
+        [Test]
+        public async Task GenerationTest_UseApiResponses_UseCancellationTokens()
         {
-            var snapshotsPath = string.Format(_snapshotsPath, testProjectName);
-
-            if (Directory.Exists(snapshotsPath))
+            var options = new GenerateClientOptions
             {
-                Directory.Delete(snapshotsPath, recursive: true);
-            }
-
-            Directory.CreateDirectory(snapshotsPath);
-
-            var generatedFiles = Directory.EnumerateFiles(_outPath, "*", new EnumerationOptions { RecurseSubdirectories = true });
-
-            foreach (var generatedFile in generatedFiles)
-            {
-                var relativePath = Path.GetRelativePath(_outPath, generatedFile);
-                File.Move(generatedFile, Path.Combine(snapshotsPath, relativePath + ".snap"));
-            }
-
-            throw new Exception("Don't forget to disable snapshot regeneration.");
-        }
-
-        private static void AssertSnapshotMatch(string testProjectName)
-        {
-            if (_regenerateSnapshots)
-            {
-                RegenerateSnapshots(testProjectName);
-            }
-
-            var snapshotsPath = string.Format(_snapshotsPath, testProjectName);
-            var generatedFiles = Directory.EnumerateFiles(_outPath, "*", new EnumerationOptions { RecurseSubdirectories = true });
-
-            foreach (var generatedFile in generatedFiles)
-            {
-                var relativePath = Path.GetRelativePath(_outPath, generatedFile);
-                var snapshotPath = Path.Combine(snapshotsPath, relativePath + ".snap");
-
-                Assert.That(snapshotPath, Does.Exist, $"Unexpected file generated ({relativePath})");
-
-                var actualContent = File.ReadAllText(generatedFile);
-                var expectedContent = File.ReadAllText(snapshotPath);
-
-                Assert.That(actualContent, Is.EqualTo(expectedContent).Using<string>(IgnoreLineEndings),
-                    $"File mismatch (${relativePath})");
-            }
-
-            static bool IgnoreLineEndings(string s1, string s2)
-            {
-                s1 = Regex.Replace(s1, "(\r|\n|\r\n)", "\r");
-                s2 = Regex.Replace(s2, "(\r|\n|\r\n)", "\r");
-                return s1 == s2;
-            }
+                InputPath = string.Format(_inputPath,"TestWebApi.MinimalApi"),
+                UseApiResponses = true,
+                AddCancellationTokenParameters = true,
+                OutPath = _outPath,
+                Namespace = "Test.Name.Space",
+            };
+        
+            Program.CreateClient(options);
+        
+            Assert.That(() => Project.FromPath(_outProjectPath).Build(), Throws.Nothing);
+            await Verifier.VerifyDirectory(_outPath);
         }
     }
 }
